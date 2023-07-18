9eb60b6a0d4e8ae0c500bf914258b7e4bdd86bec
Gitmaxd <131803031+Gitmaxd@users.noreply.github.com>
2023-07-18 14:15:17 -0700

    x

--- app/page.tsx
+++ app/page.tsx
@@ -35,11 +35,11 @@ export default function Home() {
 			<div className="hidden w-screen h-px animate-glow md:block animate-fade-right bg-gradient-to-r from-zinc-300/0 via-zinc-300/50 to-zinc-300/0" />
 			<div className="my-16 text-center animate-fade-in">
 				<h2 className="text-sm text-zinc-500 ">
-					Hi, my name is Andreas, I'm building serverless and open source
-					solutions at{" "}
+					Hi, my name is Rick, I'm a full stack developer building serverless and open source
+					solutions anf founder of {" "}
 					<Link
 						target="_blank"
-						href="https://upstash.com"
+						href="https://slicie.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
 						Upstash
@@ -49,12 +49,12 @@ export default function Home() {
 					and working on{" "}
 					<Link
 						target="_blank"
-						href="https://unkey.dev"
+						href="https://agifront.com"
 						className="underline duration-500 hover:text-zinc-300"
 					>
-						unkey.dev
+						AGI Front
 					</Link>{" "}
-					at night.
+					for research.
 				</h2>
 			</div>
 		</div>
